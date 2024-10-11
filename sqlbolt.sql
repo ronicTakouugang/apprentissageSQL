e2c751b5be156e131890cadba4c9569609e50dd8
igor lorris <igorlorris@gmail.com>
2024-10-11 14:37:50 +0100

    finished lesson1-5 + review

--- sqlbolt.sql
+++ sqlbolt.sql
@@ -1,7 +1,6 @@
 -- Lesson 1
 
 -- Exo1
-
 -- 1
 SELECT title FROM movies
 -- 2
@@ -14,7 +13,6 @@ SELECT title,year FROM movies;
 SELECT * FROM movies;
 
 -- Exo2
-
 -- 1
 SELECT * FROM movies WHERE id="6";
 -- 2
@@ -28,7 +26,6 @@ SELECT id,title,year FROM movies
 WHERE id < 6
 
 -- Exo3
-
 --1
 SELECT * FROM movies
 WHERE Title LIKE ("%Toy Story%")
@@ -43,3 +40,49 @@ SELECT * FROM movies
 WHERE Title LIKE ("%WALL-%")
 
 --Exo4
+--1
+SELECT DISTINCT director FROM movies
+ORDER BY director ASC
+--2
+SELECT id,title,year
+FROM movies
+ORDER BY year DESC
+-- limit 4 Vs code does not recognises this but the code is correct
+-- 4
+SELECT id,title
+FROM movies
+ORDER BY Title ASC
+-- limit 5
+--5
+SELECT id,title
+FROM movies
+ORDER BY Title ASC
+-- limit 5 OFFSET 5
+
+-- REVIEW LESSON 1
+-- 1
+SELECT City,Country, Population FROM north_american_cities
+WHERE Country = "Canada";
+-- 2
+SELECT City,Country,Latitude
+FROM north_american_cities
+WHERE Country = "United States"
+ORDER BY Latitude DESC;
+--3
+SELECT City,Longitude 
+FROM north_american_cities
+WHERE Longitude < -87.629798
+ORDER BY Longitude ASC;
+--4
+SELECT City, Country, Population
+FROM north_american_cities
+WHERE country="Mexico"
+ORDER BY Population DESC
+-- LIMIT 2;
+--5
+SELECT City, Country, Population
+FROM north_american_cities
+WHERE Country = "United States"
+ORDER BY Population DESC
+-- LIMIT 2 OFFSET 2;
+
