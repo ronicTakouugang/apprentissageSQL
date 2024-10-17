651715ea269d43a13df3cb0aedf354d90a224ac7
igor lorris <igorlorris@gmail.com>
2024-10-17 20:28:17 +0100

    Finished lesson 12

--- sqlbolt.sql
+++ sqlbolt.sql
@@ -173,5 +173,9 @@ SELECT director, COUNT(*) AS number_of_movies
 FROM movies
 GROUP BY director;
 -- 2
-
+SELECT director, SUM(domestic_sales + international_sales) as Cumulative_sales_from_all_movies
+FROM movies
+    INNER JOIN boxoffice
+        ON movies.id = boxoffice.movie_id
+GROUP BY director;
 
